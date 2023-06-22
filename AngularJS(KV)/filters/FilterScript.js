1eb72d3917e0c58cf68aa3012b1d9d33593afa5f
MounikaPrathushaBatchu <mounikabatchu5@gmail.com>
2023-06-22 12:05:55 +0530

    seperating CSS and HTML

--- AngularJS(KV)/filters/FilterScript.js
+++ AngularJS(KV)/filters/FilterScript.js
@@ -42,29 +42,29 @@
 
 // searching based on 2 columns only
 var app = angular
-                .module("myModule",[])
-                .controller("myController",function($scope){
-                    var employees = [
-                        { name : "Ben",dateOfBirth : new Date("November 23 1980"),gender : "Male",salary : 5500.78,city:"London" },
-                        { name : "Sara",dateOfBirth : new Date("May 05 1970"),gender : "Female",salary : 5800.12,city:"Chennai" },
-                        { name : "Mark",dateOfBirth : new Date("August 15 1974"),gender : "Male",salary : 6500.38,city:"Hyderabad" },
-                        { name : "Pam",dateOfBirth : new Date("October 27 1979"),gender : "Female",salary : 5500.786,city:"Mumbai" },
-                        { name : "Todd",dateOfBirth : new Date("December 30 1983"),gender : "Male",salary : 6200.41,city:"London" }
-                    ];
-                    $scope.employees = employees;
+    .module("myModule", [])
+    .controller("myController", function ($scope) {
+        var employees = [
+            { name: "Ben", dateOfBirth: new Date("November 23 1980"), gender: "Male", salary: 5500.78, city: "London" },
+            { name: "Sara", dateOfBirth: new Date("May 05 1970"), gender: "Female", salary: 5800.12, city: "Chennai" },
+            { name: "Mark", dateOfBirth: new Date("August 15 1974"), gender: "Male", salary: 6500.38, city: "Hyderabad" },
+            { name: "Pam", dateOfBirth: new Date("October 27 1979"), gender: "Female", salary: 5500.786, city: "Mumbai" },
+            { name: "Todd", dateOfBirth: new Date("December 30 1983"), gender: "Male", salary: 6200.41, city: "London" }
+        ];
+        $scope.employees = employees;
 
-                    $scope.search = function(item) {
-                        if($scope.searchText == undefined) {
-                            return true;
-                        } else {
-                            if(item.name.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 
-                                || item.city.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1) {
-                                return true;
-                            }
-                        }
-                        // return false;
-                    }
-                });
+        $scope.search = function (item) {
+            if ($scope.searchText == undefined) {
+                return true;
+            } else {
+                if (item.name.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1
+                    || item.city.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1) {
+                    return true;
+                }
+            }
+            // return false;
+        }
+    });
 
 
 // Custom filter
