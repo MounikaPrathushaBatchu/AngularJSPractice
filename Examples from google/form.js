c6b8a38cb7db6ac6d97e4f89666f2bc74e9e98c1
MounikaPrathushaBatchu <mounikabatchu5@gmail.com>
2023-06-26 17:38:49 +0530

    Form in AngularJS

---
+++ Examples from google/form.js
@@ -0,0 +1,9 @@
+var mainapp = angular.module("mainApp",[])
+                    .controller("studentController",function($scope) {
+                        $scope.reset = function () {
+                            $scope.firstName = "Mouni";
+                            $scope.lastName = "A";
+                            $scope.email = "mounia1@gmail.com"
+                        }
+                        $scope.reset();
+                    });
