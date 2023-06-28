4892a1941921348884aa8567146dac94a862dbb6
MounikaPrathushaBatchu <mounikabatchu5@gmail.com>
2023-06-28 10:36:48 +0530

    Services in AngularJS

--- Examples from google/Service.js
+++ Examples from google/Service.js
@@ -1,10 +1,56 @@
+// Services
+// var mainApp = angular.module("mainApp", []);
+
+// mainApp.factory('MathService', function () {
+//     var factory = {};
+
+//     factory.multiply = function (a, b) {
+//         return a * b
+//     }
+//     return factory;
+// });
+
+// mainApp.service('CalcService', function (MathService) {
+//     this.square = function (a) {
+//         return MathService.multiply(a, a);
+//     }
+// });
+
+// mainApp.controller('CalcController', function ($scope, CalcService) {
+//     $scope.square = function () {
+//         $scope.resultOne = CalcService.square($scope.number);
+//     }
+// });
+// mainApp.controller('MathController', function ($scope, MathService) {
+//     $scope.multiply = function (a, b) {
+//         $scope.resultTwo = MathService.multiply($scope.number1, $scope.number2)
+//     }
+// });
+
+
+// Dependency Injection
 var mainApp = angular.module("mainApp", []);
+
+mainApp.config(function ($provide) {
+    $provide.provider('MathService', function () {
+        this.$get = function () {
+            var factory = {};
+
+            factory.multiply = function (a, b) {
+                return a * b;
+            }
+            return factory;
+        };
+    });
+});
+
+mainApp.value("defaultInput", 5);
 
 mainApp.factory('MathService', function () {
     var factory = {};
 
     factory.multiply = function (a, b) {
-        return a * b
+        return a * b;
     }
     return factory;
 });
@@ -15,11 +61,15 @@ mainApp.service('CalcService', function (MathService) {
     }
 });
 
-mainApp.controller('CalcController', function ($scope, CalcService) {
+mainApp.controller('CalcController', function ($scope, CalcService, defaultInput) {
+    $scope.number = defaultInput;
+    $scope.result = CalcService.square($scope.number);
+
     $scope.square = function () {
         $scope.resultOne = CalcService.square($scope.number);
     }
 });
+
 mainApp.controller('MathController', function ($scope, MathService) {
     $scope.multiply = function (a, b) {
         $scope.resultTwo = MathService.multiply($scope.number1, $scope.number2)
